fe9ce271fe9669138ab79f15fb2c504ea0e236c6
niaki <nina.zizakic@ugent.be>
2020-03-27 11:19:49 +0100

    WIP5: calculate SIFT on a single patch by creating a keypoint manually

--- PythonSIFT/testing_pysift.py
+++ PythonSIFT/testing_pysift.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import pysift
 from matplotlib import pyplot as plt
 from functools import cmp_to_key
 import pickle
@@ -13,6 +12,8 @@ from pysift import generateBaseImage, computeNumberOfOctaves, generateGaussianKe
     generateDoGImages, findScaleSpaceExtrema, removeDuplicateKeypoints, convertKeypointsToInputImageSize, generateDescriptors, \
     computeKeypointsWithOrientations, isPixelAnExtremum, localizeExtremumViaQuadraticFit, compareKeypoints, unpackOctave
 
+from testing_pysift_utils import rotate_image_without_resize, pickle_keypoint_with_descriptor, unpickle_keypoint_with_descriptor
+
 
 # default params
 sigma = 1.6
@@ -22,67 +23,9 @@ image_border_width = 5
 contrast_threshold = 0.04
 
 
-def rotate_image_without_resize(image, angle):
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    return result
-
-
-def rotate_image_with_resize(image, angleInDegrees):
-    h, w = image.shape[:2]
-    img_c = (w / 2, h / 2)
-
-    rot = cv2.getRotationMatrix2D(img_c, angleInDegrees, 1)
-
-    rad = math.radians(angleInDegrees)
-    sin = math.sin(rad)
-    cos = math.cos(rad)
-    b_w = int((h * abs(sin)) + (w * abs(cos)))
-    b_h = int((h * abs(cos)) + (w * abs(sin)))
-
-    rot[0, 2] += ((b_w / 2) - img_c[0])
-    rot[1, 2] += ((b_h / 2) - img_c[1])
-
-    outImg = cv2.warpAffine(image, rot, (b_w, b_h), flags=cv2.INTER_LINEAR)
-    return outImg
-
-
-def rotate_image_around_keypoint(image, keypoint):
-    image_center = tuple(np.array(keypoint.pt, dtype=np.int32))
-    rot_mat = cv2.getRotationMatrix2D(image_center, keypoint.angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    return result
-
-
-def pickle_keypoint_with_descriptor(keypoint, descriptor, keypoint_index):
-    temp = (keypoint.pt, keypoint.size, keypoint.angle, keypoint.response, keypoint.octave, keypoint.class_id, descriptor)
-
-    pickle_file_path = 'zimnica/pickled_keypoint' + str(keypoint_index) + '_with_descriptor' + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.pickle'
-    try:
-        pickle.dump(temp, open(pickle_file_path, "wb"))
-    except Exception as e:
-        print("Problem while trying to pickle: ", str(e))
-
-
-def unpickle_keypoint_with_descriptor(pickle_file_path):
-    try:
-        # point, size, angle, response, keypoint.octave, keypoint.class_id, descriptor = pickle.load(open(pickle_file_path, "rb"))
-        temp = pickle.load(open(pickle_file_path, "rb"))
-        keypoint = cv2.KeyPoint(x=temp[0][0], y=temp[0][1], _size=temp[1], _angle=temp[2], _response=temp[3], _octave=temp[4], _class_id=temp[5])
-        print(temp[0][0], temp[0][1], temp[1], temp[2], temp[3], temp[4], temp[5])
-        print(temp[6])
-        descriptor = temp[6]
-        return keypoint, descriptor
-
-    except Exception as e:
-        print("Problem while trying to unpickle: ", str(e))
-        return None
-
-
 # query image
 image = cv2.imread('box.png', 0)
-use_pickled = False
+use_pickled = True
 
 # Compute SIFT keypoints and descriptors
 
@@ -95,6 +38,7 @@ dog_images = generateDoGImages(gaussian_images)
 
 if not use_pickled:
 
+    ######
     # keypoints = findScaleSpaceExtrema(gaussian_images, dog_images, num_intervals, sigma, image_border_width)
 
     print('Finding scale-space extrema...')
@@ -120,7 +64,7 @@ if not use_pickled:
                             for keypoint_with_orientation in keypoints_with_orientations:
                                 keypoints.append(keypoint_with_orientation)
 
-
+    ######
     # keypoints = removeDuplicateKeypoints(keypoints)
 
     print('Removing duplicate keypoints...')
@@ -139,7 +83,7 @@ if not use_pickled:
             unique_keypoints.append(next_keypoint)
     keypoints = unique_keypoints
 
-
+    ######
     # keypoints = convertKeypointsToInputImageSize(keypoints)
 
     print('Converting keypoints to input image size...')
@@ -151,8 +95,8 @@ if not use_pickled:
         converted_keypoints.append(keypoint)
     keypoints = converted_keypoints
 
-
-    print('Computing desctiptors...')
+    ######
+    print('Generating desctiptors...')
     descriptors = generateDescriptors(keypoints, gaussian_images)
 
 
@@ -182,13 +126,7 @@ else:
 
 
 
-# for every image from the folder:
-# # convert it to gray
-# # find all the keypoints
-# # for each keypoint:
 
-
-# rotate the image around it by its angle
 
 
 # image_rotated = rotate_image_with_resize(image, keypoint.angle)
@@ -197,52 +135,60 @@ else:
 # TODO keep in mind that in opencv the order is reversed!!
 patch_centre_x = int(keypoint.pt[1])
 patch_centre_y = int(keypoint.pt[0])
-patch_radius = int(math.ceil(keypoint.size / 2))
-patch_diameter = patch_radius * 2
+patch_diameter = int(2 * math.floor(keypoint.size / 2) + 1)  # rounding it to the nearest odd number
+patch_radius = (patch_diameter - 1) // 2
 
 print(patch_centre_x, patch_centre_y, patch_diameter)
 
+patch = image[patch_centre_x - patch_diameter: patch_centre_x + patch_diameter, patch_centre_y - patch_diameter: patch_centre_y + patch_diameter]  # using the diameter and not the radius to get a larger patch
 
-patch = image[patch_centre_x - patch_diameter: patch_centre_x + patch_diameter, patch_centre_y - patch_diameter: patch_centre_y + patch_diameter]
-print(image.shape)
-print(patch_centre_x - patch_diameter)  # TODO this could go out of the border and create an error!
-print(patch_centre_x + patch_diameter)
-print(patch_centre_y - patch_diameter)
-print(patch_centre_y + patch_diameter)
+# just printing
+print("image shape", image.shape)
+print("patch shape", patch.shape)
+print("patch diameter", patch_diameter)
+print("patch radius", patch_radius)
+# TODO this could go out of the border and create an error!
+print(patch_centre_x - patch_diameter, patch_centre_x + patch_diameter)
+print(patch_centre_y - patch_diameter, patch_centre_y + patch_diameter)
 plt.imshow(patch, cmap="gray")
 plt.show()
 
-patch_rotated = rotate_image_without_resize(patch, 360 - keypoint.angle)
-plt.imshow(patch_rotated, cmap="gray")
-plt.show()
-print(patch_rotated.shape)
-
-patch_rotated_cropped = patch_rotated[patch_rotated.shape[0] // 2 - patch_radius: patch_rotated.shape[0] // 2 + patch_radius + 1,
-                                      patch_rotated.shape[1] // 2 - patch_radius: patch_rotated.shape[1] // 2 + patch_radius + 1]
-
-plt.imshow(patch_rotated_cropped, cmap="gray")
-plt.show()
-print(patch_rotated_cropped.shape)
-
-imageio.imsave('images/patch' + str(keypoint_index) + '_rotated_cropped.png', patch_rotated_cropped)
-print(descriptor)
 
 
-# extract and save as a patch
-# calculate its SIFT and save as file
-# make sure that if it’s calculated like that the rotation of the keypoint is 0
 
-plt.show(block=True)
-plt.interactive(False)
+
+
+
+
+
+
+
+
+
+######
+# # rotate the patch (we're not there yet)
+# patch_rotated = rotate_image_without_resize(patch, 360 - keypoint.angle)
+# plt.imshow(patch_rotated, cmap="gray")
+# plt.show()
+# print(patch_rotated.shape)
+#
+# patch_rotated_cropped = patch_rotated[patch_rotated.shape[0] // 2 - patch_radius: patch_rotated.shape[0] // 2 + patch_radius + 1,
+#                                       patch_rotated.shape[1] // 2 - patch_radius: patch_rotated.shape[1] // 2 + patch_radius + 1]
+#
+# plt.imshow(patch_rotated_cropped, cmap="gray")
+# plt.show()
+# print(patch_rotated_cropped.shape)
+#
+# imageio.imsave('images/patch' + str(keypoint_index) + '_rotated_cropped.png', patch_rotated_cropped)
+# print(descriptor)
+#
+#
+#
+# plt.show(block=True)
+# plt.interactive(False)
 
 
 print('=====================')
 
 
 
-# create a keypoint of proper size, angle, etc
-# calculate sift and compare the values
-
-
-
-#compare the sift from this descriptor and the other one
--- PythonSIFT/testing_pysift_utils.py
+++ PythonSIFT/testing_pysift_utils.py
@@ -1,8 +1,13 @@
 """
-    Written in Python 2, to be used with Jupyter Notebook files that use Python 2 (conda environment 'cv')
+    Should be compatible with both Python 2 and 3
+    (first couple of methods used with Jupyter Notebook files that use Python 2 (conda environment 'cv',
+    methods from rotate_image_with_resize to be used in testing_pysift.py)
 """
 import numpy as np
 import cv2 as cv
+import pickle, datetime
+import math
+
 
 def unpack_octave(keypoint):
     """Compute octave, layer, and scale from a keypoint
@@ -61,4 +66,55 @@ def rotate_image_without_resize(image, angle):
     rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
     return result
+
+
+def rotate_image_with_resize(image, angleInDegrees):
+    h, w = image.shape[:2]
+    img_c = (w / 2, h / 2)
+
+    rot = cv.getRotationMatrix2D(img_c, angleInDegrees, 1)
+
+    rad = math.radians(angleInDegrees)
+    sin = math.sin(rad)
+    cos = math.cos(rad)
+    b_w = int((h * abs(sin)) + (w * abs(cos)))
+    b_h = int((h * abs(cos)) + (w * abs(sin)))
+
+    rot[0, 2] += ((b_w / 2) - img_c[0])
+    rot[1, 2] += ((b_h / 2) - img_c[1])
+
+    outImg = cv.warpAffine(image, rot, (b_w, b_h), flags=cv.INTER_LINEAR)
+    return outImg
+
+
+def rotate_image_around_keypoint(image, keypoint):
+    image_center = tuple(np.array(keypoint.pt, dtype=np.int32))
+    rot_mat = cv.getRotationMatrix2D(image_center, keypoint.angle, 1.0)
+    result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+    return result
+
+
+def pickle_keypoint_with_descriptor(keypoint, descriptor, keypoint_index):
+    temp = (keypoint.pt, keypoint.size, keypoint.angle, keypoint.response, keypoint.octave, keypoint.class_id, descriptor)
+
+    pickle_file_path = 'zimnica/pickled_keypoint' + str(keypoint_index) + '_with_descriptor' + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.pickle'
+    try:
+        pickle.dump(temp, open(pickle_file_path, "wb"))
+    except Exception as e:
+        print("Problem while trying to pickle: ", str(e))
+
+
+def unpickle_keypoint_with_descriptor(pickle_file_path):
+    try:
+        # point, size, angle, response, keypoint.octave, keypoint.class_id, descriptor = pickle.load(open(pickle_file_path, "rb"))
+        temp = pickle.load(open(pickle_file_path, "rb"))
+        keypoint = cv.KeyPoint(x=temp[0][0], y=temp[0][1], _size=temp[1], _angle=temp[2], _response=temp[3], _octave=temp[4], _class_id=temp[5])
+        print(temp[0][0], temp[0][1], temp[1], temp[2], temp[3], temp[4], temp[5])
+        print(temp[6])
+        descriptor = temp[6]
+        return keypoint, descriptor
+
+    except Exception as e:
+        print("Problem while trying to unpickle: ", str(e))
+        return None
 
